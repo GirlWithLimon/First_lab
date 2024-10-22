f1b59bd5b094464d8b72e2a2f0485c252b9a2f50
Dari <156703870+GirlWithLimon@users.noreply.github.com>
2024-10-22 13:43:54 +0000

    1Lb.py

--- 1Lb.py
+++ 1Lb.py
@@ -1,6 +1,6 @@
 #!/home/vboxuser/PycharmProjects/pythonProject/.venv/bin/python
 import random
 import sys
+sys.stderr=open('errors.txt','w')
 A=random.randint(-10,10)
 print (A)
-sys.stderr=open('errors.txt','w')
