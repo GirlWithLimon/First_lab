35a163334e9d2bc22628a55a7f78e817d3ae96a4
Dari <156703870+GirlWithLimon@users.noreply.github.com>
2024-10-22 13:45:00 +0000

    2Lb.py

--- 2Lb.py
+++ 2Lb.py
@@ -1,6 +1,7 @@
 #!/home/vboxuser/PycharmProjects/pythonProject/.venv/bin/python
 import random
 import sys
+sys.stderr=open('errors.txt','w')
 A=int(input("Write A: "))
 B=random.randint(-10,10)
 print("B=",B)
@@ -8,4 +9,3 @@ if B==0:
     print("Error, B=0")
 else:
     print ("A/B=",A/B)
-sys.stderr=open('errors.txt','w')
