e0ba5092402a8160525b30af9696016795042f5f
Dari <156703870+GirlWithLimon@users.noreply.github.com>
2024-12-13 18:47:56 +0300

    Delete 3Lb.py

--- 3Lb.py
+++
@@ -1,9 +0,0 @@
-#!/home/vboxuser/PycharmProjects/pythonProject/.venv/bin/python
-import sys
-sys.stderr=open('errors.txt','w')
-A=int(input("Write number: "))
-A=A**2
-print("A^2=",A)
-res=open("output.txt",'w')
-res.write(str(A))
-res.close()
